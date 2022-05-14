d4e97b264952c527fa81d85eee924e20a95a159a
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2022-05-14 22:30:56 +0200

    Fix Windows archive creation

--- build_package.sh
+++ build_package.sh
@@ -3,14 +3,17 @@ mkdir -p ~/platformio_packages
 for d in */ ; do
     cd "$d"
     simavr_tar="simavr.tar.gz"
-    simavr_zip="*.zip"
+    simavr_zip="simavr.zip"
     simavr_extracted="simavr"
+    echo "Testing $d"
+    ls
+    mv *.zip simavr.zip
     if test -f "$simavr_tar"; then
         echo "tar gz files existed"
         tar xfv "$simavr_tar" --one-top-level
     elif test -f "$simavr_zip"; then
         echo "zip file existed"
-        unzip $simavr_zip
+        unzip $simavr_zip -d $simavr_extracted
     else
         echo "No archive existed"
         continue
@@ -25,7 +28,7 @@ for d in */ ; do
         ("Linux 32-bit") echo "\"linux_x86\"";;
         ("Windows 64-bit") echo "\"windows_amd64\"";;
         ("Windows 32-bit") echo "\"windows_x86\"";;
-        ("Mac OS Intel 64-bit") echo "\"daring_x86_64\", \"darwin_arm64\"";;
+        ("Mac OS Intel 64-bit") echo "\"darwin_x86_64\", \"darwin_arm64\"";;
         ("Linux AArch64") echo "\"linux_aarch64\"";;
         ("Linux ARMv6l") echo "\"linux_armv6l\"";;
         ("Linux ARMv7l") echo "\"linux_armv7l\",\"linux_armv8l\"";;
@@ -52,11 +55,10 @@ for d in */ ; do
   }
 }
 EOF
-    #~/.platformio/penv/bin/platformio package pack -o ".." "$simavr_extracted"
+    #~/.platformio/penv/bin/platformio package pack -o ~/platformio_packages "$simavr_extracted"
     pio package pack -o ~/platformio_packages "$simavr_extracted"
     cd ..
 done
-echo "Back to $(pwd)"
 cd ~
-ls -R platformio_packages
+ls -lh platformio_packages
 pwd
